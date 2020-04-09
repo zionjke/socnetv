0b7aad1f2b4c63bf46964d8a1fcff22e2ddf7104
zionjke <vincere802@gmail.com>
2020-04-09 18:56:07 +0300

    add NavLink +

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () => {
     return (
         <nav className={styles.nav}>
             <div className={`${styles.item} ${styles.active}`}>
-                <NavLink to="/profile">Profile</NavLink>
+                <NavLink to="/profile" activeClassName={styles.active}>Profile</NavLink>
             </div>
             <div className={styles.item}>
-                <NavLink to="/dialogs">Dialogs</NavLink>
+                <NavLink to="/dialogs"  activeClassName={styles.active}>Dialogs</NavLink>
             </div>
             <div className={styles.item}>
                 <NavLink to="/news">News</NavLink>
