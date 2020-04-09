776fe581d6c07c8bedfad1075e9f59695516f894
zionjke <vincere802@gmail.com>
2020-04-09 16:49:23 +0300

    add router-dom module

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,19 +5,19 @@ const Navbar = () => {
     return (
         <nav className={styles.nav}>
             <div className={`${styles.item} ${styles.active}`}>
-                <a>Profile</a>
+                <a href="/profile">Profile</a>
             </div>
             <div className={styles.item}>
-                <a>Messages</a>
+                <a href="/messages">Messages</a>
             </div>
             <div className={styles.item}>
-                <a>News</a>
+                <a href="/news">News</a>
             </div>
             <div className={styles.item}>
-                <a>Music</a>
+                <a href="/music">Music</a>
             </div>
             <div className={styles.item}>
-                <a>Settings</a>
+                <a href="/settings">Settings</a>
             </div>
 
         </nav>
