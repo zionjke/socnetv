7b13b2aaa50b6f8b8aa474a068b784fdea13a9a1
artem_vasiliev <vincere802@gmail.com>
2020-04-10 17:22:25 +0300

    create layout Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,8 +4,26 @@ import styles from './Dialogs.module.css'
 
 const Dialogs  = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                <div className={styles.dialog + " " + styles.active}>
+                    Dimych
+                </div>
+                <div className={styles.dialog}>
+                    Andrey
+                </div>
+                <div className={styles.dialog}>
+                    Sveta
+                </div>
+                <div className={styles.dialog}>
+                    Sasha
+                </div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>Hi</div>
+                <div className={styles.message}>How is your It?</div>
+                <div className={styles.message}>Yo</div>
+            </div>
         </div>
     );
 }
