920f0556aa63243a933771782c00307867b22c7a
zionjke <vincere802@gmail.com>
2020-04-23 12:57:20 +0300

    refactoring++

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,18 +4,21 @@ import Post from "./Post/Post";
 
 const MyPosts = (props) => {
 
-
-
+    let newPostElement = React.createRef()
     let postsEl = props.posts.map(p => <Post message={p.message} likeCount={p.likeCount}/>);
+    let addPost = () => {
+        let text = newPostElement.current.value
+        alert(text)
+    };
 
     return (
         <div className={styles.postsBlock}>
             <h3>My posts</h3>
                 <div>
-                    <textarea></textarea>
+                    <textarea  ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Отправить</button>
+                    <button onClick={addPost}>Отправить</button>
                 </div>
             <div className={styles.posts}>
                 {postsEl}
