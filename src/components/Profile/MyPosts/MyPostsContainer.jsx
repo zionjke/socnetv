b1767291a2a43f536e670a31ad7c990718e6ae4b
artem_vasiliev <vincere802@gmail.com>
2020-06-15 11:16:16 +0300

    add withRouter

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -4,31 +4,6 @@ import MyPosts from "./MyPosts";
 import {connect} from "react-redux";
 
 
-// const MyPostsContainer = () => {
-//
-//     return (
-//         <StoreContext.Consumer>
-//             {
-//             (store) => {
-//                 let state = store.getState();
-//
-//                 let addPost = () => {
-//                     store.dispatch(addPostActionCreator());
-//                 };
-//
-//                 let onPostChange = (text) => {
-//                     store.dispatch(updateNewPostTextActionCreator(text))
-//                 };
-//
-//                 return <MyPosts updateNewPostText={onPostChange}
-//                                 addPost={addPost}
-//                                 posts={state.profilePage.posts}
-//                                 newPostText={state.profilePage.newPostText}/>
-//             }
-//             }
-//         </StoreContext.Consumer>
-//     );
-// };
 
 let mapStateToProps = (state) => {
     return {
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,12 +3,17 @@ import Profile from "./Profile";
 import * as axios from "axios";
 import {connect} from "react-redux";
 import {setUserProfile} from "../../redux/profile-reducer";
+import {withRouter} from "react-router-dom";
 
 
 class ProfileContainer extends React.Component{
 
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`).then(response => {
+        let userID = this.props.match.params.userId;
+        if (!userID) {
+            userID = 2
+        }
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userID}`).then(response => {
             this.props.setUserProfile(response.data);
         })
     }
@@ -28,5 +33,6 @@ const mapStateToProps = (state) => {
     }
 };
 
+let WithUrlDataContainerComponent =  withRouter(ProfileContainer);
 
-export default connect(mapStateToProps,{setUserProfile})(ProfileContainer)
+export default connect(mapStateToProps,{setUserProfile})(WithUrlDataContainerComponent)
